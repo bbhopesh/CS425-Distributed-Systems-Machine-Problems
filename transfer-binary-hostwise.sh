44fc8fffcb1c0f2270ed3d6657a75588fedf4039
Bhopesh Bassi <bbhopesh@gmail.com>
2017-06-10 21:55:00 -0500

    Rename repo and project.

--- transfer-binary-hostwise.sh
+++ transfer-binary-hostwise.sh
@@ -7,17 +7,17 @@ remoteSsh="ssh ${remoteUser}@${hostName}"
 # Create remote directory.
 $remoteSsh << EOF
 	cd # start from home.
-	mkdir -p CS425MP1
-	cd CS425MP1
+	mkdir -p CS425-Distributed-Systems-Machine-Problems
+	cd CS425-Distributed-Systems-Machine-Problems
 	rm -rf *
 	cd
 EOF
 # copy file.
-scp build/distributions/CS425-MP1.tar $remoteSsh:"/home/$remoteUser/CS425MP1"
+scp build/distributions/CS425-Distributed-Systems-Machine-Problems.tar $remoteSsh:"/home/$remoteUser/CS425-Distributed-Systems-Machine-Problems"
 
 # Remote unpack tar
 $remoteSsh << EOF
-	cd ~/CS425MP1
-	tar -xf CS425-MP1.tar
+	cd ~/CS425-Distributed-Systems-Machine-Problems
+	tar -xf CS425-Distributed-Systems-Machine-Problems.tar
 	
 EOF
